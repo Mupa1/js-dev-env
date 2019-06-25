5d581472c8e85c85998076ac8ecf1005454c8202
Mupa M <mupasmail@gmail.com>
2019-06-25 13:36:08 +0300

    add srcServer file

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let path = require('path');
+let open = require('open');
+
+let port = 5500;
+let app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
