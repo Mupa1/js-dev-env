4301bfcf5bb308325106029380dcfba0b380af36
Mupa M <mupasmail@gmail.com>
2019-06-27 10:46:09 +0300

    add userApi file

---
+++ src/api/userApi.js
@@ -0,0 +1,17 @@
+import 'whatwg-fetch';
+
+export function getUsers() {
+  return get('users');
+}
+
+function get(url) {
+  return fetch(url).then(onSuccess, onError);
+}
+
+function onSuccess(response) {
+  return response.json();
+}
+
+function onError(error) {
+  console.log(error); // eslint-disable-line no-console
+}
