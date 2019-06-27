a126a3165b8fa0ba303e59492e32afe3b140e9ec
Mupa M <mupasmail@gmail.com>
2019-06-27 15:27:56 +0300

    import getBaseUrl to userApi file

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,14 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 export function getUsers() {
   return get('users');
 }
 
 function get(url) {
-  return fetch(url).then(onSuccess, onError);
+  return fetch(baseUrl + url).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
