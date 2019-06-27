043b4e361b0cf3f69710363171084b043316acd8
Mupa M <mupasmail@gmail.com>
2019-06-27 16:07:09 +0300

    add delete function to userApi file

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -7,8 +7,21 @@ export function getUsers() {
   return get('users');
 }
 
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
 function get(url) {
   return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+// Can't call func delete since reserved word.
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
