ab2cf2bbecbd3952d4dfb228f2c797503f75a13b
Mupa M <mupasmail@gmail.com>
2019-06-27 11:07:43 +0300

    modify srcServer file

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -22,8 +22,8 @@ app.get('/', function(req, res) {
 app.get('/users', function(req, res) {
   res.json([
     {"id": 1, "firstName": "Mupa", "lastName": "M'mbetsa", "email": "mupasmail@gmail.com"},
-    {"id": 1, "firstName": "Maureen", "lastName": "Kavutha", "email": "maureensmail@gmail.com"},
-    {"id": 1, "firstName": "Penny", "lastName": "Wambui", "email": "pennysmail@gmail.com"}
+    {"id": 2, "firstName": "Maureen", "lastName": "Kavutha", "email": "maureensmail@gmail.com"},
+    {"id": 3, "firstName": "Penny", "lastName": "Wambui", "email": "pennysmail@gmail.com"}
   ]);
 });
 
